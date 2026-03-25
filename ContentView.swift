c0f82af7bb6e3485e1d766653277b67d7fd5fd33
EuniceA-tech8 <eunice.aduagyei01@gmail.com>
2026-03-25 17:21:42 -0400

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -19,37 +19,73 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 20) {
             Rectangle()
-                .fill(Color(red: redIsOn ? redAmount / 255.0 : 0, green: greenIsOn ? greenAmount / 255.0 : 0, blue: blueIsOn ? blueAmount / 255.0 : 0))
-                .frame(width: 250, height: 100)
-                .border(Color.black)
+                .foregroundStyle(Color(red: redIsOn ? redAmount / 255.0 : 0, green: greenIsOn ? greenAmount / 255.0 : 0, blue: blueIsOn ? blueAmount / 255.0 : 0))
+                .frame(height: 200)
+                .border(Color.black, width: 3)
                 .shadow(radius: 10)
+            
+           
             
             Text("Intensity:    Red: \(Int(redAmount) * 100 / 255), Blue: \(Int(blueAmount * 100 / 255)), Green: \(Int(greenAmount) * 100 / 255)")
             
-            Slider(value: $redAmount, in: 0...255)
-                .tint(.red)
-            Slider(value: $blueAmount, in: 0...255)
+            HStack {
+                
+                //Red Toggle
+                Toggle(isOn: $redIsOn, label:{ Text("Activate Red")
+                    
+                }).labelsHidden()
+                    .tint(.red)
+
+                //Red Slider
+                Slider(value: $redAmount, in: 0...255)
+                    .tint(.red)
+            }
+            HStack{
+                //Blue Toggle
+                Toggle(isOn: $blueIsOn, label:{ Text("Activate Blue")
+                }).labelsHidden()
+                    .tint(.blue)
+              
+                //Blue Slider
+                Slider(value: $blueAmount, in: 0...255)
+                    .tint(.blue)
+                
+            }
+            
+            HStack{
+                //Green Toggle
+                Toggle(isOn: $greenIsOn, label:{ Text("Activate Green")
+                    }).labelsHidden()
+                    .tint(.green)
+                   
+                //Green Slider
+                Slider(value: $greenAmount, in: 0...255)
+                    .tint(.green)
+               
+                
+            }
+            
+            Button("Reset"){
+                redIsOn = true
+                blueIsOn = true
+                greenIsOn = true
+                redAmount = 255.0
+                blueAmount = 255.0
+                greenAmount = 255.0
+            }.buttonStyle(.borderedProminent)
                 .tint(.blue)
-            Slider(value: $greenAmount, in: 0...255)
-                .tint(.green)
             
-            //Text("Toggle Color")
-            Toggle("Activate Red", isOn: $redIsOn)
-                .foregroundColor(redIsOn ? .red : .primary)
-            //Text("Toggle Color")
-            Toggle("Activate Blue", isOn: $blueIsOn)
-                .foregroundColor(blueIsOn ? .blue : .primary)
-            Toggle("Activate Green", isOn: $blueIsOn)
-            //Text("Toggle Color")
-                .foregroundColor(greenIsOn ? .green : .primary)
-            
-    }  
+    }
+        
         .padding()
     }
     
 }
     
 
+
+
 #Preview {
     ContentView()
 }
+
